9b20e22211a85aee9a28427b17583a25d53572e2
caduroriz <ceduardo.roriz@gmail.com>
2024-09-30 09:08:28 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Carlos Eduardo Roriz on 30/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.blue)
+            Text("Hello, CS139p!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
