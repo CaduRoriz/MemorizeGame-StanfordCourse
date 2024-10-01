16c8e0aaf8dd6c933e28a3f8ba8741bd59425a9a
caduroriz <ceduardo.roriz@gmail.com>
2024-10-01 15:23:58 -0300

    otimizando código, container co grid, adição de botões

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,57 +1,76 @@
-//
 //  ContentView.swift
 //  Memorize
-//
 //  Created by Carlos Eduardo Roriz on 30/09/24.
-//
-
 import SwiftUI
 
-struct ContentView: View { //behaves like a... a "tipacao" da struct significa que ela vai se comportar como(o tipo da struct, no caso View)
-    var body: some View {
+    struct ContentView: View {
+        let emojis: [String] = ["👻", "🎃", "🕷️", "👹", "🕸️", "💀", "🧛🏻", "🧌", "😱", "🦇", "🍭", "☠️"]
+        @State var cardCount: Int = 4
         
-        HStack{
-            CardView( emoji: "🎃" )
-            CardView()
-            CardView(emoji: "🎃")
-            CardView()
+        var body: some View {
+            VStack{
+                ScrollView{
+                    cards
+                }
+                
+                Spacer()
+                cardAdjustersStack
+            }.padding( 20)
+            
+            }
+        
+        var cards: some View {
+            return LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]){
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(emoji: emojis[index])
+                }
+            }
         }
-        .padding()
         
+        var cardAdjustersStack: some View {
+            HStack{
+                cardCountBuilder(acao: {if cardCount > 1 { cardCount -= 1 }}, simbol: "rectangle.stack.fill.badge.minus")
+                Spacer()
+                cardCountBuilder(acao: {if cardCount < emojis.count { cardCount += 1 }}, simbol: "rectangle.stack.fill.badge.plus")
+            }
+        }
         
+        func cardCountBuilder(acao: @escaping () -> Void, simbol: String) -> some View {
+            return Button(action: {
+                acao()
+            }, label: {
+                Image(systemName: simbol).font(.largeTitle)
+            })
+        }
+
     }
-    
-}
 
-
-struct CardView: View{
-    @State var isFaceUp = false // uma visualuzacao nao pode ser alterada, é imutavel! entao adicionamos o @State para a var que na verdade é um ponteiro pra variavel, de forma que o ponteiri em si nunca muda de fato, apenas o conteudo para que ele esta apontando... a logica da carta virada ou nao ficara no back-end e nao aqui... esse tipo de ferramenta só deve ser utilizada para pequenas coisas, como o estado dessa carta e ainda sim esse estado constara no back.
-    @State var showAlert = false
-    var emoji = "👻"
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp{
+    struct CardView: View {
+        @State var isFaceUp = true
+        let emoji: String
+        
+        var body: some View {
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(emoji).font(.largeTitle)
-
-            } else {
-                base.fill()
+                
+                base.fill(.opacity(isFaceUp ? 0 : 1))
+                
             }
-        }
-        .foregroundColor(.orange)
-        .padding(.horizontal, 5)
-        .onTapGesture {
-            print("tapped")
-            isFaceUp = !isFaceUp // oa tipos, assim como o Bool, sao strcuts em swift, e por serem structs possuem funcoes atribuidas, dessa forma essa alteracao de estado pode ser via toggle,assim:  isFaceUp.toggle()
+            .foregroundColor(.orange)
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
+            .aspectRatio(2/3, contentMode: .fit)
+        
         }
     }
-}
 
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-
-#Preview {
-    ContentView()
-    
-}
